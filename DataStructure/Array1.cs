c2de9c48c61649e88bc52c8df94b189338a4cfdf
XZY <506670541@qq.com>
2021-10-21 14:27:46 +0800

    参考视频将《C#数据结构与算法》--2020 最新精讲版:2-6 装箱和拆箱  制作完毕

---
+++ DataStructure/Array1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+    class Array1
+    {
+        /// <summary>int[]:数组</summary>
+        private int[] data;
+
+        /// <summary>int:数组中存储元素个数</summary>
+        private int N;
+
+        //构造函数:
+        public Array1(int capacity)
+        {
+            data = new int[capacity];
+            N = 0;
+        }
+
+        //构造函数
+ 
+
+        //方法:获取data.Length
+        public int Capacity
+        {
+            get { return data.Length; }
+        }
+
+        //方法:获取N
+        public int Count
+        {
+            get { return N; }
+        }
+
+        //是否是空
+        public bool IsEmpty
+        {
+            get { return N == 0; }
+        }
+
+        ////数组添加元素：在数组中间空间位置添加元素
+        //public void Add(int index,int e)
+        //{
+        //    if (index<0||index>N)
+        //        throw new ArgumentException("数组索引越界！");
+
+        //    if (N==data.Length)
+        //        throw new ArgumentException("数组已满!");
+
+        //    //从后向前，每一个往后挪一个
+        //    for (int i =N-1; i >=index; i--)               
+        //        data[i + 1] = data[i]; //将当前空间数据给后面一个空间，实现将从index开始的元素每一个往后挪一个空间位置
+
+        //    data[index] = e;
+        //    N++;
+        //}
+
+        /// <summary>
+        /// 视频编号: 2-5 动态数组
+        /// 功能：数组添加元素：在数组中间空间位置添加元素
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="e"></param>
+        public void Add(int index, int e)
+        {
+            if (index < 0 || index > N)
+                throw new ArgumentException("数组索引越界！");
+
+            if (N == data.Length)
+                ResetCapacity(2 * data.Length);//( 视频编号: 2-5 动态数组:增加内容)因为需要动态数组，所以当增加元素超过数组长度后，需要扩容相当于之前2倍大小的新数组
+                                               //throw new ArgumentException("数组已满!");
+
+            //从后向前，每一个往后挪一个
+            for (int i = N - 1; i >= index; i--)
+                data[i + 1] = data[i]; //将当前空间数据给后面一个空间，实现将从index开始的元素每一个往后挪一个空间位置
+
+            data[index] = e;
+            N++;
+        }
+
+        //数组添加元素:在末尾空间位置添加元素
+        public void AddLast(int e)
+        {
+            Add(N, e);
+        }
+
+        //数组添加元素:在首位空间位置添加元素
+        public void AddFirst(int e)
+        {
+            Add(0, e);
+        }
+
+        /// <summary>
+        /// 视频编号: 2-3 获取、修改、打印
+        /// 功能:得到某个索引的元素
+        /// </summary>
+        /// <param name="index">数组中索引编号</param>
+        /// <returns></returns>
+        public int Get(int index)
+        {
+            if (index<0||index>=N)
+            {
+                throw new ArgumentException("数组索引越界！");
+            }
+            return data[index];
+        }
+
+        /// <summary>
+        /// 视频编号: 2-3 获取、修改、打印
+        /// 功能：得到第一个索引的元素
+        /// </summary>
+        /// <returns></returns>
+        public int GetFirst()
+        {
+            return Get(0);
+        }
+
+        /// <summary>
+        /// 视频编号: 2-3 获取、修改、打印
+        /// 功能：得到数组最后一个索引的元素
+        /// </summary>
+        /// <returns></returns>
+        public int GetLast()
+        {
+            return Get(N - 1);
+        }
+
+        /// <summary>
+        ///视频编号: 2-3 获取、修改、打印
+        /// 功能:修改索引位置的元素
+        /// </summary>
+        /// <param name="index">准备在什么索引进行修改</param>
+        /// <param name="newE">准备将制定位置的元素修改什么数值</param>
+        public void Set(int index,int newE)
+        {
+            if (index<0||index>=N)
+            {
+                throw new ArgumentException("索引越界!");
+            }
+            data[index] = newE;
+        }
+
+        /// <summary>
+        /// 视频编号: 2-4 包含、搜索、删除
+        /// 功能：检查数组是否包含输入的元素
+        /// </summary>
+        /// <param name="e">输入的元素</param>
+        /// <returns>存在:true| 不存在:false</returns>
+        public bool Contains(int e)
+        {
+            for (int i = 0; i <N; i++)
+            {
+                if (data[i]==e)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 视频编号: 2-4 包含、搜索、删除
+        /// 功能：获取元素在数组第一次从前往后第几个位置
+        /// </summary>
+        /// <param name="e">检查的元素</param>
+        /// <returns>如果输入的元素存在返回元素在元素中的索引   如果输入的元素不存在数组就返回-1</returns>
+        public int IndexOf(int e)
+        {
+            for (int i = 0; i <N; i++)
+            {
+                if (data[i]==e)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        ///// <summary>
+        /////  视频编号: 2-4 包含、搜索、删除
+        ///// 功能：删除指定索引的元素
+        ///// </summary>
+        ///// <param name="index">删除元素的索引</param>
+        ///// <returns>删除索引的元素内容</returns>
+        //public int RemoveAt(int index)
+        //{
+        //    if (index<0||index>=N)
+        //        throw new ArgumentException("索引越界!");
+
+        //    int del = data[index];//删除位置的元素内容
+
+        //    for (int i = index+1; i <=N-1; i++)
+        //        data[i -1] = data[i];//这里时候联想：删除数组中的元素:动画演示
+
+        //    N--;
+        //    data[N] = default(int);
+
+        //    return del;
+        //}
+
+        /// <summary>
+        ///  视频编号: 2-4 包含、搜索、删除
+        ///  视频编号: 2-5 动态数组
+        /// 功能：删除指定索引的元素
+        /// </summary>
+        /// <param name="index">删除元素的索引</param>
+        /// <returns>删除索引的元素内容</returns>
+        public int RemoveAt(int index)
+        {
+            if (index < 0 || index >= N)
+                throw new ArgumentException("索引越界!");
+
+            int del = data[index];//删除位置的元素内容
+
+            for (int i = index + 1; i <= N - 1; i++)
+                data[i - 1] = data[i];//这里时候联想：删除数组中的元素:动画演示
+
+            N--;
+            data[N] = default(int);
+
+
+            // ( 视频编号: 2-5 动态数组:增加内容)想想：动画演示:数组缩容因为当删除数组某个元素后，
+            // 如果数组长度没有之前的四分之一长，那么就可以将其缩容一半，减少内存开支，
+            //并不是只有之前一半长久缩容，而是四分之一，减少内存空间不断的缩容和扩容
+            if (N==data.Length/4)
+                ResetCapacity(data.Length / 2);
+
+            return del;
+        }
+
+        /// <summary>
+        ///  视频编号: 2-4 包含、搜索、删除
+        /// 功能：删除第一个索引的元素
+        /// </summary>
+        /// <returns></returns>
+        public int RemoveFirst()
+        {
+            return RemoveAt(0);
+        }
+
+        /// <summary>
+        ///  视频编号: 2-4 包含、搜索、删除
+        /// 功能：删除数组最后一个索引的元素
+        /// </summary>
+        /// <returns></returns>
+        public int RemoveLast()
+        {
+            return RemoveAt(N - 1);
+        }
+
+        /// <summary>
+        ///  视频编号: 2-4 包含、搜索、删除
+        /// 功能：删除指定索引位置的元素
+        /// </summary>
+        /// <param name="e">输入参数:移除数组从左向右第一个元素为e的元素</param>
+        public void Remove(int e)
+        {
+            int index = IndexOf(e);
+            if (index!=-1)
+                RemoveAt(index);
+        }
+
+        /// <summary>
+        /// 视频编号: 2-5 动态数组
+        /// 功能：重新开辟一个内容空间，想想 动画演示:数组扩容，就会理解这样写的思路
+        /// </summary>
+        /// <param name="newCapacity"></param>
+        private void ResetCapacity(int newCapacity)
+        {
+            int[] newData = new int[newCapacity];//新建一个数组，大小是形参
+            for (int i = 0; i <N; i++)
+            {
+                newData[i] = data[i];//将之前的数组数据都赋值给新的数组
+            }
+            data = newData;//将新创建的数组赋值给全局变量data，这样就实现了数组data的扩容。
+        }
+
+        /// <summary>
+        /// 功能：因为Console.writeLine()调用的是父类的ToString，直接是类的名字，所以需要重写toString：方便输出数组内部数据，
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            StringBuilder res = new StringBuilder();
+            res.Append(string.Format("Array1: count={0} capacity={1}\n", N, data.Length));
+            res.Append("[");
+            for (int i = 0; i <N; i++)
+            {
+                res.Append(data[i]);
+                if (i!=N-1)
+                    res.Append(",  ");
+            }
+            res.Append("]");
+            return res.ToString();
+        }
+
+    }
+}
---
+++ DataStructure/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region[视频编号: 2-1 什么是动态数组]
+            ////1.静态数组:不越界的
+            //int[] arr = new int[10];
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    arr[i] = i;
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.Read();
+
+            ////2.静态数组:越界的
+            //int[] arr = new int[10];
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    arr[i] = i;
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.Read();
+
+            ////3.动态数组:添加内容和初始化时候数量一致
+            //ArrayList a = new ArrayList(10);
+            //for (int i = 0; i <10; i++)
+            //{
+            //    a.Add(i);
+            //    Console.Write(a[i] + " ");
+            //}
+            //Console.Read();
+
+            ////4.动态数组：添加内容和初始化时候数量一致
+            //ArrayList a = new ArrayList(10);
+            //for (int i = 0; i < 15; i++)
+            //{
+            //    a.Add(i);
+            //    Console.Write(a[i] + " ");
+            //}
+            //Console.Read();
+
+            ////5.动态数组:List
+            //List<int> l = new List<int>(10);
+            //for (int i = 0; i <15; i++)
+            //{
+            //    l.Add(i);
+            //    Console.Write(l[i] + " ");
+            //}
+            //Console.Read();
+            #endregion
+
+            #region[视频编号: 2-3 获取、修改、打印]
+            //Array1 a = new Array1(20);
+            //for (int i = 0; i <10; i++)
+            //{
+            //    a.AddLast(i);
+            //}
+            //Console.WriteLine(a);
+
+            //a.AddFirst(66);
+            //Console.WriteLine(a);
+
+            //a.Add(2, 88);
+            //Console.WriteLine(a);
+
+            //Console.WriteLine(a.GetFirst());
+            //Console.WriteLine(a.GetLast());
+            //Console.WriteLine(a.Get(1));
+
+            //a.Set(1, 6666);
+            //Console.WriteLine(a );
+
+            //Console.Read();
+            #endregion
+
+            #region[视频编号: 2-4 包含、搜索、删除]
+            //Array1 a = new Array1(20);
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    a.AddLast(i);
+            //}
+            //Console.WriteLine(a);
+
+            //a.AddFirst(66);
+            //Console.WriteLine(a);
+
+            //a.Add(2, 77);
+            //Console.WriteLine(a);
+
+            ////删除前数据内容：[66,  0,  77,  1,  2,  3,  4,  5,  6,  7,  8,  9]
+            //a.RemoveAt(2);
+            //a.RemoveFirst();
+            //a.RemoveLast();
+            //a.Remove(4);
+            //Console.WriteLine(a);
+            ////删除前数据内容:[0,  1,  2,  3,  5,  6,  7,  8]
+
+            //Console.Read();
+            #endregion
+
+            #region[视频编号: 2-5 动态数组:增加内容]
+            //Array1 a = new Array1(10);
+            //for (int i = 0; i <10; i++)
+            //{
+            //    a.AddLast(i);
+            //}
+            //Console.WriteLine(a);
+
+            //a.AddLast(55);
+            //Console.WriteLine(a);
+
+            //for (int i = 0; i <6; i++)
+            //{
+            //    a.RemoveLast();
+            //    Console.WriteLine(a);
+            //}
+            //Console.Read();
+            #endregion
+
+            #region[视频编号:2-6 装箱和拆箱]
+            //int n = 10000000;
+            //Stopwatch t1 = new Stopwatch();
+            //Stopwatch t2 = new Stopwatch();
+            //Stopwatch t3 = new Stopwatch();
+            //Stopwatch t4 = new Stopwatch();
+
+            ////测试1：泛型List+int值类型
+            //t1.Start();
+            //List<int> l = new List<int>();
+            //for (int i = 0; i <n; i++)
+            //{
+            //    l.Add(i);//int:值类型+List:不发生装箱
+            //    int x = l[i];//int:值类型+List:不发生拆箱
+            //}
+            //t1.Stop();
+            //Console.WriteLine("测试1：泛型List+int值类型 花费时间:" + t1.ElapsedMilliseconds + "ms");
+
+            ////测试2:非泛型ArrayList+int值类型
+            //t2.Start();
+            //ArrayList a = new ArrayList();
+            //for (int i = 0; i <n; i++)
+            //{
+            //    a.Add(i);//发生装箱：非泛型ArrayList+int值类型
+            //    int x = (int)a[i];//发生拆箱：非泛型ArrayList+int值类型
+            //}
+            //t2.Stop();
+            //Console.WriteLine("测试2:非泛型ArrayList+int值类型 花费时间:" + t2.ElapsedMilliseconds + "ms");
+
+            ////测试3：泛型List+引用类型对象string
+            //t3.Start();
+            //List<string> l2 = new List<string>();
+            //for (int i = 0; i <n; i++)
+            //{
+            //    l2.Add("X");//不发生装箱
+            //    string x = l2[i];//不发生拆箱
+            //}
+            //t3.Stop();
+            //Console.WriteLine("测试3：泛型List+引用类型对象string:花费时间:" + t3.ElapsedMilliseconds+"ms");
+
+            ////测试4:非泛型ArrayList+ 引用类型string
+            //t4.Start();
+            //ArrayList a2 = new ArrayList();
+            //for (int i = 0; i <n; i++)
+            //{
+            //    a2.Add("X");//不发生装箱
+            //    string s = (string)a2[i];//不发生拆箱
+            //}
+            //t4.Stop();
+            //Console.WriteLine("测试4:非泛型ArrayList+ 引用类型string 花费时间:" + t4.ElapsedMilliseconds + "ms");
+
+            //Console.Read();
+            #endregion
+        }
+    }
+}
