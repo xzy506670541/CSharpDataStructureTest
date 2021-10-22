bdf856b4577b62f27de66e8cfa902b6d22203b03
XZY <506670541@qq.com>
2021-10-22 15:11:18 +0800

    完成实操:4-4 数组栈、链表栈性能比较
    20211022-15:11

---
+++ DataStructure/LinkedList1Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+    /// <summary>[功能：泛型的链表栈，继承 泛型的IStack 接口]  [Author:XZY Time:20211022 视频编号：4-3 链表栈] </summary>
+    class LinkedList1Stack<E>: IStack<E>
+    {
+        /// <summary>[功能：私有对象]  [Author:XZY Time:20211022 视频编号：4-3 链表栈] </summary>
+        private LinkedList1<E> list;
+
+        /// <summary>[功能：构造函数]  [Author:XZY Time:20211022 视频编号：4-3 链表栈] </summary>
+        public LinkedList1Stack()
+        {
+            list = new LinkedList1<E>();
+        }
+
+        /// <summary>[功能：得到链表栈的节点个数]  [Author:XZY Time:20211022 视频编号：4-3 链表栈] </summary>
+        public int Count { get { return list.Count; } }
+
+        /// <summary>[功能：得到链表栈是否为空]  [Author:XZY Time:20211022 视频编号：4-3 链表栈] </summary>
+        public bool IsEmpty { get { return list.IsEmpty; } }
+
+        /// <summary>[功能：查看栈顶节点(头部节点)]  [Author:XZY Time:20211022 视频编号：4-3 链表栈] </summary>
+        public E Peek()
+        {
+            return list.GetFirst();
+        }
+
+        /// <summary>[功能：删除栈顶节点(头部节点)]  [Author:XZY Time:20211022 视频编号：4-3 链表栈] </summary>
+        public E Pop()
+        {
+            return list.RemoveFirst();
+        }
+
+        /// <summary>[功能：添加链接头部节点)]  [Author:XZY Time:20211022 视频编号：4-3 链表栈] </summary>
+        public void Push(E e)
+        {
+             list.AddFirst(e);
+        }
+
+        public override string ToString()
+        {
+            return "Stack :top " + list.ToString();
+        }
+
+    }
+}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -3,278 +3,355 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 
+//namespace DataStructure
+//{
+//    class Program
+//    {
+// static void Main(string[] args)
+//   {
+#region[视频编号: 2-1 什么是动态数组]
+////1.静态数组:不越界的
+//int[] arr = new int[10];
+//for (int i = 0; i < 10; i++)
+//{
+//    arr[i] = i;
+//    Console.Write(arr[i] + " ");
+//}
+//Console.Read();
+
+////2.静态数组:越界的
+//int[] arr = new int[10];
+//for (int i = 0; i < 15; i++)
+//{
+//    arr[i] = i;
+//    Console.Write(arr[i] + " ");
+//}
+//Console.Read();
+
+////3.动态数组:添加内容和初始化时候数量一致
+//ArrayList a = new ArrayList(10);
+//for (int i = 0; i <10; i++)
+//{
+//    a.Add(i);
+//    Console.Write(a[i] + " ");
+//}
+//Console.Read();
+
+////4.动态数组：添加内容和初始化时候数量一致
+//ArrayList a = new ArrayList(10);
+//for (int i = 0; i < 15; i++)
+//{
+//    a.Add(i);
+//    Console.Write(a[i] + " ");
+//}
+//Console.Read();
+
+////5.动态数组:List
+//List<int> l = new List<int>(10);
+//for (int i = 0; i <15; i++)
+//{
+//    l.Add(i);
+//    Console.Write(l[i] + " ");
+//}
+//Console.Read();
+#endregion
+
+#region[视频编号: 2-3 获取、修改、打印]
+//Array1 a = new Array1(20);
+//for (int i = 0; i <10; i++)
+//{
+//    a.AddLast(i);
+//}
+//Console.WriteLine(a);
+
+//a.AddFirst(66);
+//Console.WriteLine(a);
+
+//a.Add(2, 88);
+//Console.WriteLine(a);
+
+//Console.WriteLine(a.GetFirst());
+//Console.WriteLine(a.GetLast());
+//Console.WriteLine(a.Get(1));
+
+//a.Set(1, 6666);
+//Console.WriteLine(a );
+
+//Console.Read();
+#endregion
+
+#region[视频编号: 2-4 包含、搜索、删除]
+//Array1 a = new Array1(20);
+//for (int i = 0; i < 10; i++)
+//{
+//    a.AddLast(i);
+//}
+//Console.WriteLine(a);
+
+//a.AddFirst(66);
+//Console.WriteLine(a);
+
+//a.Add(2, 77);
+//Console.WriteLine(a);
+
+////删除前数据内容：[66,  0,  77,  1,  2,  3,  4,  5,  6,  7,  8,  9]
+//a.RemoveAt(2);
+//a.RemoveFirst();
+//a.RemoveLast();
+//a.Remove(4);
+//Console.WriteLine(a);
+////删除前数据内容:[0,  1,  2,  3,  5,  6,  7,  8]
+
+//Console.Read();
+#endregion
+
+#region[视频编号: 2-5 动态数组:增加内容]
+//Array1 a = new Array1(10);
+//for (int i = 0; i <10; i++)
+//{
+//    a.AddLast(i);
+//}
+//Console.WriteLine(a);
+
+//a.AddLast(55);
+//Console.WriteLine(a);
+
+//for (int i = 0; i <6; i++)
+//{
+//    a.RemoveLast();
+//    Console.WriteLine(a);
+//}
+//Console.Read();
+#endregion
+
+#region[视频编号:2-6 装箱和拆箱]
+//int n = 10000000;
+//Stopwatch t1 = new Stopwatch();
+//Stopwatch t2 = new Stopwatch();
+//Stopwatch t3 = new Stopwatch();
+//Stopwatch t4 = new Stopwatch();
+
+////测试1：泛型List+int值类型
+//t1.Start();
+//List<int> l = new List<int>();
+//for (int i = 0; i < n; i++)
+//{
+//    l.Add(i);//int:值类型+List:不发生装箱
+//    int x = l[i];//int:值类型+List:不发生拆箱
+//}
+//t1.Stop();
+//Console.WriteLine("测试1：泛型List+int值类型 花费时间:" + t1.ElapsedMilliseconds + "ms");
+
+////测试2:非泛型ArrayList+int值类型
+//t2.Start();
+//ArrayList a = new ArrayList();
+//for (int i = 0; i < n; i++)
+//{
+//    a.Add(i);//发生装箱：非泛型ArrayList+int值类型
+//    int x = (int)a[i];//发生拆箱：非泛型ArrayList+int值类型
+//}
+//t2.Stop();
+//Console.WriteLine("测试2:非泛型ArrayList+int值类型 花费时间:" + t2.ElapsedMilliseconds + "ms");
+
+////测试3：泛型List+引用类型对象string
+//t3.Start();
+//List<string> l2 = new List<string>();
+//for (int i = 0; i < n; i++)
+//{
+//    l2.Add("X");//不发生装箱
+//    string x = l2[i];//不发生拆箱
+//}
+//t3.Stop();
+//Console.WriteLine("测试3：泛型List+引用类型对象string:花费时间:" + t3.ElapsedMilliseconds + "ms");
+
+////测试4:非泛型ArrayList+ 引用类型string
+//t4.Start();
+//ArrayList a2 = new ArrayList();
+//for (int i = 0; i < n; i++)
+//{
+//    a2.Add("X");//不发生装箱
+//    string s = (string)a2[i];//不发生拆箱
+//}
+//t4.Stop();
+//Console.WriteLine("测试4:非泛型ArrayList+ 引用类型string 花费时间:" + t4.ElapsedMilliseconds + "ms");
+
+//Console.Read();
+#endregion
+
+#region[视频编号:2-7 使用泛型技术]
+//int[] n = { 1,2,3,4,5,6,7};
+//Array1<int> a = new Array1<int>();
+//for (int i = 0; i <n.Length; i++)
+//{
+//    a.AddLast(n[i]);//将数组n添加到a泛型中
+//}
+//Console.WriteLine(a);
+
+//string[] s = {"a","b","c","d"};
+//Array1<string> a2 =new Array1<string>();
+//for (int i = 0; i <s.Length; i++)
+//{
+//    a2.AddLast(s[i]);//将数组s添加到a2泛型中
+//}
+//Console.WriteLine(a2);
+
+//Console.ReadLine();     
+#endregion
+
+#region[3-3 获取、修改、包含、打印]
+//LinkedList1<int> l = new LinkedList1<int>();
+//for (int i = 0; i < 5; i++)
+//{
+//    //for循环添加元素
+//    l.AddFirst(i);
+//    Console.WriteLine(l);
+//}
+
+////末尾添加元素
+//l.AddLast(666);
+//Console.WriteLine(l);
+
+////指定位置添加元素
+//l.Add(2, 999);
+//Console.WriteLine(l);
+
+////指定位置修改元素
+//l.Set(2, 1000);
+//Console.WriteLine(l);
+
+//Console.Read();
+#endregion
+
+#region[3-4 删除链表中的结点]
+//LinkedList1<int> l = new LinkedList1<int>();
+//for (int i = 0; i < 5; i++)
+//{
+//    //for循环添加元素
+//    l.AddFirst(i);
+//    Console.WriteLine(l);
+//}
+
+////末尾添加元素
+//l.AddLast(666);
+//Console.WriteLine(l);
+
+////指定位置添加元素
+//l.Add(2, 999);
+//Console.WriteLine(l);
+
+////指定位置修改元素
+//l.Set(2, 1000);
+//Console.WriteLine(l);
+
+//l.RemoveAt(2);
+//Console.WriteLine(l);
+
+//l.RemoveFirst();
+//Console.WriteLine(l);
+
+//l.RemoveLast();
+//Console.WriteLine(l);
+
+//l.Remove(0);
+//Console.WriteLine(l);
+
+//Console.Read();
+#endregion
+
+#region [视频编号 4-2 数组栈 ]
+//Array1Stack<int> stack = new Array1Stack<int>();
+
+////测试:数组栈末尾添加元素
+//for (int i = 0; i <5; i++)
+//{ 
+//    stack.Push(i);
+//    Console.WriteLine(stack);
+//}
+
+//stack.Pop();
+//Console.WriteLine(stack);
+
+//Console.ReadLine();
+#endregion
+
+#region[视频编号4-3 链表栈]
+//LinkedList1Stack<int> stack = new LinkedList1Stack<int>();
+//for (int i = 0; i <5; i++)
+//{
+//    stack.Push(i);
+//    Console.WriteLine(stack);
+//}
+
+//stack.Pop();
+//Console.WriteLine(stack);
+
+//Console.ReadLine();
+#endregion
+
+
+//}
+
+
+//    }
+//}
+
+#region[视频编号 4-4 数组栈、链表栈性能比较]
+
 namespace DataStructure
 {
     class Program
     {
+        /// <summary>
+        ///[功能：测试栈耗时]  [Author:XZY Time:20211022 视频编号：4-3 链表栈]
+        /// </summary>
+        /// <param name="stack">栈</param>
+        /// <param name="N">栈里面的长度</param>
+        /// <returns></returns>
+        public static long TestStack(IStack<int> stack,int N)
+        {
+            Stopwatch t = new Stopwatch();
+            t.Start();
+            for (int i = 0; i < N; i++)
+                stack.Push(i);
+
+            for (int i = 0; i <N; i++)
+                stack.Pop();
+
+            t.Stop();
+            return t.ElapsedMilliseconds;
+        }
+
         static void Main(string[] args)
         {
-            #region[视频编号: 2-1 什么是动态数组]
-            ////1.静态数组:不越界的
-            //int[] arr = new int[10];
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    arr[i] = i;
-            //    Console.Write(arr[i] + " ");
-            //}
-            //Console.Read();
+            int N = 1000000;
 
-            ////2.静态数组:越界的
-            //int[] arr = new int[10];
-            //for (int i = 0; i < 15; i++)
-            //{
-            //    arr[i] = i;
-            //    Console.Write(arr[i] + " ");
-            //}
-            //Console.Read();
+            //测试:1:数组栈的耗时
+            Array1Stack<int> array1Stack = new Array1Stack<int>();//不扩容的方式创建对象
+            //Array1Stack<int> array1Stack = new Array1Stack<int>(N);//扩容的方式创建对象
+            long t1 = TestStack(array1Stack, N);
+            Console.WriteLine("array1Stack‘time=" + t1 + "ms");
 
-            ////3.动态数组:添加内容和初始化时候数量一致
-            //ArrayList a = new ArrayList(10);
-            //for (int i = 0; i <10; i++)
-            //{
-            //    a.Add(i);
-            //    Console.Write(a[i] + " ");
-            //}
-            //Console.Read();
+            //测试：2：链表栈的耗时
+            LinkedList1Stack<int> linkedList1Stack = new LinkedList1Stack<int>();
+            long l2 = TestStack(linkedList1Stack, N);
+            Console.WriteLine("linkedList1Stack‘time=" + l2 + "ms");
 
-            ////4.动态数组：添加内容和初始化时候数量一致
-            //ArrayList a = new ArrayList(10);
-            //for (int i = 0; i < 15; i++)
-            //{
-            //    a.Add(i);
-            //    Console.Write(a[i] + " ");
-            //}
-            //Console.Read();
-
-            ////5.动态数组:List
-            //List<int> l = new List<int>(10);
-            //for (int i = 0; i <15; i++)
-            //{
-            //    l.Add(i);
-            //    Console.Write(l[i] + " ");
-            //}
-            //Console.Read();
-            #endregion
-
-            #region[视频编号: 2-3 获取、修改、打印]
-            //Array1 a = new Array1(20);
-            //for (int i = 0; i <10; i++)
-            //{
-            //    a.AddLast(i);
-            //}
-            //Console.WriteLine(a);
-
-            //a.AddFirst(66);
-            //Console.WriteLine(a);
-
-            //a.Add(2, 88);
-            //Console.WriteLine(a);
-
-            //Console.WriteLine(a.GetFirst());
-            //Console.WriteLine(a.GetLast());
-            //Console.WriteLine(a.Get(1));
-
-            //a.Set(1, 6666);
-            //Console.WriteLine(a );
-
-            //Console.Read();
-            #endregion
-
-            #region[视频编号: 2-4 包含、搜索、删除]
-            //Array1 a = new Array1(20);
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    a.AddLast(i);
-            //}
-            //Console.WriteLine(a);
-
-            //a.AddFirst(66);
-            //Console.WriteLine(a);
-
-            //a.Add(2, 77);
-            //Console.WriteLine(a);
-
-            ////删除前数据内容：[66,  0,  77,  1,  2,  3,  4,  5,  6,  7,  8,  9]
-            //a.RemoveAt(2);
-            //a.RemoveFirst();
-            //a.RemoveLast();
-            //a.Remove(4);
-            //Console.WriteLine(a);
-            ////删除前数据内容:[0,  1,  2,  3,  5,  6,  7,  8]
-
-            //Console.Read();
-            #endregion
-
-            #region[视频编号: 2-5 动态数组:增加内容]
-            //Array1 a = new Array1(10);
-            //for (int i = 0; i <10; i++)
-            //{
-            //    a.AddLast(i);
-            //}
-            //Console.WriteLine(a);
-
-            //a.AddLast(55);
-            //Console.WriteLine(a);
-
-            //for (int i = 0; i <6; i++)
-            //{
-            //    a.RemoveLast();
-            //    Console.WriteLine(a);
-            //}
-            //Console.Read();
-            #endregion
-
-            #region[视频编号:2-6 装箱和拆箱]
-            //int n = 10000000;
-            //Stopwatch t1 = new Stopwatch();
-            //Stopwatch t2 = new Stopwatch();
-            //Stopwatch t3 = new Stopwatch();
-            //Stopwatch t4 = new Stopwatch();
-
-            ////测试1：泛型List+int值类型
-            //t1.Start();
-            //List<int> l = new List<int>();
-            //for (int i = 0; i < n; i++)
-            //{
-            //    l.Add(i);//int:值类型+List:不发生装箱
-            //    int x = l[i];//int:值类型+List:不发生拆箱
-            //}
-            //t1.Stop();
-            //Console.WriteLine("测试1：泛型List+int值类型 花费时间:" + t1.ElapsedMilliseconds + "ms");
-
-            ////测试2:非泛型ArrayList+int值类型
-            //t2.Start();
-            //ArrayList a = new ArrayList();
-            //for (int i = 0; i < n; i++)
-            //{
-            //    a.Add(i);//发生装箱：非泛型ArrayList+int值类型
-            //    int x = (int)a[i];//发生拆箱：非泛型ArrayList+int值类型
-            //}
-            //t2.Stop();
-            //Console.WriteLine("测试2:非泛型ArrayList+int值类型 花费时间:" + t2.ElapsedMilliseconds + "ms");
-
-            ////测试3：泛型List+引用类型对象string
-            //t3.Start();
-            //List<string> l2 = new List<string>();
-            //for (int i = 0; i < n; i++)
-            //{
-            //    l2.Add("X");//不发生装箱
-            //    string x = l2[i];//不发生拆箱
-            //}
-            //t3.Stop();
-            //Console.WriteLine("测试3：泛型List+引用类型对象string:花费时间:" + t3.ElapsedMilliseconds + "ms");
-
-            ////测试4:非泛型ArrayList+ 引用类型string
-            //t4.Start();
-            //ArrayList a2 = new ArrayList();
-            //for (int i = 0; i < n; i++)
-            //{
-            //    a2.Add("X");//不发生装箱
-            //    string s = (string)a2[i];//不发生拆箱
-            //}
-            //t4.Stop();
-            //Console.WriteLine("测试4:非泛型ArrayList+ 引用类型string 花费时间:" + t4.ElapsedMilliseconds + "ms");
-
-            //Console.Read();
-            #endregion
-
-            #region[视频编号:2-7 使用泛型技术]
-            //int[] n = { 1,2,3,4,5,6,7};
-            //Array1<int> a = new Array1<int>();
-            //for (int i = 0; i <n.Length; i++)
-            //{
-            //    a.AddLast(n[i]);//将数组n添加到a泛型中
-            //}
-            //Console.WriteLine(a);
-
-            //string[] s = {"a","b","c","d"};
-            //Array1<string> a2 =new Array1<string>();
-            //for (int i = 0; i <s.Length; i++)
-            //{
-            //    a2.AddLast(s[i]);//将数组s添加到a2泛型中
-            //}
-            //Console.WriteLine(a2);
-
-            //Console.ReadLine();     
-            #endregion
-
-            #region[3-3 获取、修改、包含、打印]
-            //LinkedList1<int> l = new LinkedList1<int>();
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    //for循环添加元素
-            //    l.AddFirst(i);
-            //    Console.WriteLine(l);
-            //}
-
-            ////末尾添加元素
-            //l.AddLast(666);
-            //Console.WriteLine(l);
-
-            ////指定位置添加元素
-            //l.Add(2, 999);
-            //Console.WriteLine(l);
-
-            ////指定位置修改元素
-            //l.Set(2, 1000);
-            //Console.WriteLine(l);
-
-            //Console.Read();
-            #endregion
-
-            #region[3-4 删除链表中的结点]
-            //LinkedList1<int> l = new LinkedList1<int>();
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    //for循环添加元素
-            //    l.AddFirst(i);
-            //    Console.WriteLine(l);
-            //}
-
-            ////末尾添加元素
-            //l.AddLast(666);
-            //Console.WriteLine(l);
-
-            ////指定位置添加元素
-            //l.Add(2, 999);
-            //Console.WriteLine(l);
-
-            ////指定位置修改元素
-            //l.Set(2, 1000);
-            //Console.WriteLine(l);
-
-            //l.RemoveAt(2);
-            //Console.WriteLine(l);
-
-            //l.RemoveFirst();
-            //Console.WriteLine(l);
-
-            //l.RemoveLast();
-            //Console.WriteLine(l);
-
-            //l.Remove(0);
-            //Console.WriteLine(l);
-
-            //Console.Read();
-            #endregion
-
-            #region [视频编号 4-2 数组栈 ]
-            Array1Stack<int> stack = new Array1Stack<int>();
-
-            //测试:数组栈末尾添加元素
-            for (int i = 0; i <5; i++)
-            { 
+            //测试：3：Stack栈 的耗时
+            Stack<int> stack = new Stack<int>(N);
+            Stopwatch t = new Stopwatch();
+            t.Start();
+            for (int i = 0; i < N; i++)
                 stack.Push(i);
-                Console.WriteLine(stack);
-            }
 
-            stack.Pop();
-            Console.WriteLine(stack);
+            for (int i = 0; i < N; i++)
+                stack.Pop();
 
-            Console.ReadLine();
-            #endregion
+            t.Stop();
+            Console.WriteLine("Stack‘time=" + t.ElapsedMilliseconds + "ms");
 
         }
     }
 }
 
+
+#endregion
